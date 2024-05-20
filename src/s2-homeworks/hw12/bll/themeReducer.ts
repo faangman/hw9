e779abaa5b8628706963ebcf6756ec3da6584c33
faangman <faang.net@gmail.com>
2024-05-19 22:04:44 -0700

    lesson hw16

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,20 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state: {themeId: number} = initState, action: changeThemeIdACType): {themeId: number} => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID': {
+            return {
+                ...state,
+                themeId: action.id
+            }
+        }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type changeThemeIdACType = ReturnType<typeof changeThemeId>
+
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id } as const) // fix any
